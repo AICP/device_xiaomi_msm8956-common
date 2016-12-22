224fb34a01e3c4eee1e86dfc97e70fbc14f7c863
TheStrix <parthbhatia98@gmail.com>
2016-12-22 18:35:01 +0530

    msm8956: Misc updates from MIUI N
     * Update and reorganize system props
     * Do not preload libNimsWrap.so
     * Update audio configs
     * Update wifi configs
    
    Change-Id: Iad53b8bc25458c1a2f6279870f12bc21311f949e

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -1,5 +1,6 @@
 #
 # Copyright (C) 2016 The CyanogenMod Project
+# Copyright (C) 2017 The LineageOS Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -102,7 +103,6 @@ BOARD_HARDWARE_CLASS += hardware/cyanogen/cmhw
 
 # CNE
 BOARD_USES_QCNE := true
-TARGET_LDPRELOAD := libNimsWrap.so
 
 # Display
 BOARD_USES_ADRENO := true
